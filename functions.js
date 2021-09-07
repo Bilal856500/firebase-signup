7e713a5fa0a4330d117140d36cd7e5e8e9276641
Ibad Shah <iamibadd>
2021-09-08 01:43:36 +0500

    Subject: Code refactored.

--- functions.js
+++ functions.js
@@ -3,74 +3,35 @@ const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
 const password = document.getElementById('Password');
 const confirmPassword = document.getElementById('ConfirmPassword');
-// const signIn = document.getElementById('writeData');
-const database = firebase.database();
-var rootRef = database.ref('users/');
+const database = firebase.database().ref('users');
 
-document.getElementById("form").addEventListener("submit", async(e) => {
-
-    e.preventDefault();
-
-    const checkUsername = checkExistingUsername(username.value);
-    console.log(checkUsername);
-});
-
-
-function checkExistingUsername(username) {
-    firebase.database().ref('users').child(username).on('value', async(snapshot) => {
-        let flag;
-        try {
-            let data = await snapshot.val().UserName;
-
-            alert('try');
-            flag = true;
-        } catch (e) {
-            alert('catch');
-            flag = false;
-        }
-        return flag;
-        // if (snapshot.exists()) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        // value = await snapshot.val(); //these 2 lines are used to fetch single data from database
-    });
+async function checkExistingUsername(username) {
+	return database.child(username).once('value').then(async (snapshot) => {
+		return snapshot.exists();
+	});
 }
-
 
 async function signUp() {
-
-    // event.preventDefault();
-    const checkUsername = await checkExistingUsername(username.value);
-    console.log(checkUsername);
-    return true;
-    database.ref('users/' + username.value).set({
-        UserName: username.value,
-        FirstName: firstName.value,
-        LastName: lastName.value,
-        Password: password.value,
-        ConfirmPassword: confirmPassword.value
-    });
-
+	return database.child(username.value).set({
+		userName: username.value,
+		firstName: firstName.value,
+		lastName: lastName.value,
+		password: password.value,
+		confirmPassword: confirmPassword.value
+	});
 }
 
-// function setRecord() {
-// rootRef.once('value', function(snapshot) {
-//     if (snapshot.exists()) {
-//         let index = 1;
-//         const data = snapshot.val();
-//         for (const key in data) {
-//             let Username = data[key].UserName;
-//             // alert(key)
-//             let FName = data[key].FirstName;
-//             let LName = data[key].LastName;
-//             let password = data[key].Password;
-//             let confirmPassword = data[key].ConfirmPassword;
-//             // validation(Username, FName, LName, password, confirmPassword, key, index)
-//             index++;
-//         }
 
-//     }
+document.getElementById("form").addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const checkUsername = await checkExistingUsername(username.value);
+	if (checkUsername) {
+		alert('Username exists!');
+	} else if (password.value !== confirmPassword.value) {
+		alert(`Passwords do not match!`);
+	} else {
+		await signUp();
+		location.reload();
+	}
 
-// })
+});
